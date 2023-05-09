0fe3493ae374852ccf47531aa444b60f4e95a038
Vina Rianti <vina.rianti@dyson.com>
2023-05-09 11:03:34 +0800

    Add scanner view controller

--- Slurpy/ViewControllers/ScannerViewController.swift
+++ Slurpy/ViewControllers/ScannerViewController.swift
@@ -1,36 +1,57 @@
-/// Copyright (c) 2022 Razeware LLC
-/// 
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-/// 
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-/// 
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-/// 
-/// This project and source code may use libraries or frameworks that are
-/// released under various Open-Source licenses. Use of those libraries and
-/// frameworks are governed by their own individual licenses.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
-
 import UIKit
+import VisionKit
 
 class ScannerViewController: UIViewController {
+  
+  var dataScanner: DataScannerViewController?
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    installDataScanner()
+  }
+  
+  func installDataScanner() {
+    guard dataScanner == nil else { return }
+    
+    let scanner = DataScannerViewController.makeDatascanner(delegate: self)
+    dataScanner = scanner
+    addChild(scanner)
+    view.pinToInside(scanner.view)
+    
+    addChild(scanner)
+    scanner.didMove(toParent: self)
+    
+    do {
+      try scanner.startScanning()
+    } catch {
+      print("Error: Unable to start scan. \(error)")
+    }
+  }
+}
+
+extension ScannerViewController: DataScannerViewControllerDelegate {
+  
+  func dataScanner(_ dataScanner: DataScannerViewController, didAdd addedItems: [RecognizedItem], allItems: [RecognizedItem]) {
+    
+  }
+  
+  func dataScanner(_ dataScanner: DataScannerViewController, didUpdate updatedItems: [RecognizedItem], allItems: [RecognizedItem]) {
+    
+  }
+  
+  func dataScanner(_ dataScanner: DataScannerViewController, didRemove removedItems: [RecognizedItem], allItems: [RecognizedItem]) {
+    
+  }
+  
+  func dataScanner(_ dataScanner: DataScannerViewController, didTapOn item: RecognizedItem) {
+    
+  }
+}
+
+extension DataScannerViewController {
+  static func makeDatascanner(delegate: DataScannerViewControllerDelegate) -> DataScannerViewController {
+    let scanner = DataScannerViewController(recognizedDataTypes: [.text()], isGuidanceEnabled: true, isHighlightingEnabled: true)
+    scanner.delegate = delegate
+    return scanner
+  }
 }
